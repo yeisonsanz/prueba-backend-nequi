b5a7bf1f6b226925ddef92f01cdeaee24a06fda7
yeisonsanz <yeisonkb@gmail.com>
2025-05-15 21:26:16 -0500

    configurando la base de datos en las properties

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.3.11"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.nequi"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // Spring Boot WebFlux
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter")
+
+    // R2DBC y MySQL
+    implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+    implementation("dev.miku:r2dbc-mysql:0.8.2.RELEASE")
+
+    // Lombok
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/main/java/com/nequi/prueba/PruebaNequiApplication.java
@@ -0,0 +1,13 @@
+package com.nequi.prueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaNequiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PruebaNequiApplication.class, args);
+    }
+
+}
